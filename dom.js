401ad88b3673677d2b727d33330c0a80eef9dcfa
Srinath20 <srinathgollapinni>
2023-09-11 11:15:49 +0530

    DOM manipulation II

--- dom.js
+++ dom.js
@@ -1,8 +1,17 @@
+console.dir(document);
+console.log(document.head);
+console.log(document.body);
+document.title = "Project";
+console.log(document.title);
+console.log(document.all);
 var heading = document.getElementById('header-title');
-console.log(heading.innerText)
-heading.innerText = 'A Plain List';
-heading.style.fontWeight = 'bold';
-heading.style.borderBottom = '2PX solid black';
-var items = document.getElementById('items');
-items[1].style.fontWeight = 'bold';
-items[1].style.innerText = '<p style="color:green">Item</p>'
+heading.innerText="Project Title";
+//heading.innerHTML="<h3>Sample Text</h3>"
+var Item = document.getElementsByClassName('list-group-item');
+Item[2].textContent = "Edited list item";
+var edit = Item[2];
+edit.style.fontWeight = 'bold';
+edit.style.backgroundColor = 'green';
+for(var i=0;i<Item.length;i++){
+    Item[i].style.fontWeight = 'bold';
+}
