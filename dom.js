a849f4ffd623ac85898e19cc54cc883ef84c9cca
Srinath20 <srinathgollapinni>
2023-09-18 09:49:01 +0530

    Traversing DOM

--- dom.js
+++ dom.js
@@ -27,3 +27,37 @@ var second = document.querySelector('li:nth-child(2)');
 second.style.backgroundColor = 'green';
 var third = document.querySelector('li:nth-child(3)');
 third.style.opacity= 0;
+
+//Traversing DOM
+//parentNode or parentElement
+var itemList = document.querySelector('.list-group');
+console.log(itemList.parentNode);
+//ChildNodes
+console.log(itemList.childNodes);//childNodes..considers line breaks but children doesn't
+console.log(itemList.children);
+//console.log(itemList.firstChild);//This also considers line breaks but firstElementChild doesn't
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.innerText = "Sample text";
+//Simlarly for lastChild and lastElementChild
+
+//nextSibling
+console.log(itemList.nextSibling);///This also considers line breaks but nextElementSibling doesn't
+console.log(itemList.nextElementSibling);
+
+//Similarly previousSibling and previousElementSibling
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
+
+//createElement, textNode and Add textNode to div
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute('titile',"HEllo");
+var newText = document.createTextNode('HEllo');
+newDiv.appendChild(newText);
+
+//Inserting createElement
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
+
